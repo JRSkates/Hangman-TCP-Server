62ca90a1de6db0c1f1ae14d7e48a07018b94119b
JRSkates <skatesproduction@gmail.com>
2025-02-07 10:07:55 +0000

    inital project setup

---
+++ server.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+
+#define PORT 4242
+#define MAX_CLIENTS 10
+#define MAX_GUESSES 6
+
+int main(void) {
+
+    return 0;
+}
